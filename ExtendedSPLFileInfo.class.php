3cfc42aeb47adf62644c44951171e33a2c0dcd11
Rob Lambell <rob@lambell.info>
2014-01-19 13:25:04 +0000

    Initial version

---
+++ ExtendedSPLFileInfo.class.php
@@ -0,0 +1,71 @@
+<?php
+
+class ExtendedSPLFileInfo extends SPLFileInfo
+{
+    /**
+     * @param $filename
+     */
+    public function __construct($filename) {
+        parent::__construct($filename);
+    }
+
+    /**
+     * @return int
+     */
+    function getDirSize()
+    {
+        if ($this->isDir()) {
+            $path = $this->getPathname();
+        } else {
+            $path = $this->getPath();
+        }
+
+        $size = 0;
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS));
+        foreach($files as $file){
+            $size += $file->getSize();
+        }
+        return $size;
+    }
+
+    /**
+     * @return int
+     */
+    function getFileMTime()
+    {
+        if ($this->isDir()) {
+            $path = $this->getPathname();
+        } else {
+            return $this->getMTime();
+        }
+
+        $lastModified = 0;
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS));
+        foreach ($files as $file) {
+            if ($file->getMTime() > $lastModified) {
+                $lastModified = $file->getMTime();
+            }
+        }
+
+        if ($lastModified < 1) {
+            $lastModified = $this->getMTime();
+        }
+
+        return $lastModified;
+    }
+
+    /**
+     * @param bool $bytes
+     * @param int  $decimals
+     * @return string
+     */
+    public function getHumanSize($bytes = false, $decimals = 2)
+    {
+        $bytes = $bytes ? $bytes : $this->getSize();
+
+        $size = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f ", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,22 @@ ExtendedSPLFileInfo
 ===================
 
 PHP class extends SPLFileInfo
+
+Methods
+-----
+
+* getDirSize() - size of directory contents (recursive) or parent directory if isFile
+
+* getFileMTime() - MTime of most recent directory contents (recursive) or file if isFile
+
+* getHumanSize() - human readable filesize, %.2f {$factor}. default from $this->getSize()
+
+Usage
+------------
+
+```php
+$iterator = new FilesystemIterator(dirname(__FILE__));
+$iterator->setInfoClass('ExtendedSPLFileInfo');
+foreach ($iterator as $file) {
+	echo $file->getFileMTime() . PHP_EOL;
+}
